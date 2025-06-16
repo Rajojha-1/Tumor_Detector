6e9fb333b66e736fb360113ce2185782bb29e35a
Raj <rajojha047@gmail.com>
2025-06-16 13:28:56 +0530

    Update app.py to use dynamic port for Render deployment

--- app.py
+++ app.py
@@ -47,6 +47,8 @@ def predict():
     else:
         return render_template('index.html', prediction=None, accuracy=None)
 
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host="0.0.0.0", port=port)
 
-if __name__ == '__main__':
-    app.run(debug=True)
